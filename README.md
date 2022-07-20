1da99097137a2d6e6e9cb86463efb4d5cb949f30
Allan Sousa <allan.sousa@portinfo.com.br>
2022-07-20 09:13:37 -0300

    Alterações no README.md

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Script automatização instalação de aplicações Ubuntu
-Script de post install do Pop!_OS_22.04
+# Automatização de instalação Pop!_Os_22.04
+Script de post install do Pop!_OS_22.04, mas os comando valem para outras distribuições baseadas no Ubuntu
 
 
 ## Como usar
 
 Apenas execute o comando abaixo no terminal
 ```shell
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Diolinux/pop-os-postinstall/main/pop-os-postinstall.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/allansousadev/Posinstall_Pop_Os_22.04/blob/main/shell_posinstall_linux_2022.sh)"
 ```
--- shell_posinstall_linux_2022.sh
+++ shell_posinstall_linux_2022.sh
@@ -2,24 +2,16 @@
 #
 # pos-os-postinstall.sh - Instalar e configura programas no Pop!_OS (20.04 LTS ou superior)
 #
-# Website:       https://diolinux.com.br
-# Autor:         Dionatan Simioni
+# GitHub:       https: https://github.com/allansousadev
+# Autor:        Allan Sousa 
 #
 # ------------------------------------------------------------------------ #
 #
 # COMO USAR?
-#   $ ./pos-os-postinstall.sh
+#   $ ./shell_posinstall_linux_2022.sh.sh
 #
 # ----------------------------- VARIÁVEIS ----------------------------- #
 set -e
-
-##URLS
-
-URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.0-1_amd64.deb?source=website"
-URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.7.2.50318-impish_amd64.deb"
-URL_SYNOLOGY_DRIVE="https://global.download.synology.com/download/Utility/SynologyDriveClient/3.0.3-12689/Ubuntu/Installer/x86_64/synology-drive-client-12689.x86_64.deb"
-
 
 ##DIRETÓRIOS E ARQUIVOS
 
@@ -42,7 +34,7 @@ apt_update(){
   sudo apt update && sudo apt dist-upgrade -y
 }
 
-# -------------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------------ #
 # -------------------------------TESTES E REQUISITOS----------------------------------------- #
 
 # Internet conectando?
@@ -76,20 +68,12 @@ sudo apt update -y
 ##DEB SOFTWARES TO INSTALL
 
 PROGRAMAS_PARA_INSTALAR=(
+  zsh
   snapd
-  winff
-  virtualbox
-  ratbagd
-  gparted
-  timeshift
-  gufw
-  synaptic
-  solaar
-  vlc
-  code
-  gnome-sushi 
   folder-color
   git
+  git
+  curl
   wget
   ubuntu-restricted-extras
  
@@ -97,60 +81,12 @@ PROGRAMAS_PARA_INSTALAR=(
 
 # ---------------------------------------------------------------------- #
 
-## Download e instalaçao de programas externos ##
-
-install_debs(){
-
-echo -e "${VERDE}[INFO] - Baixando pacotes .deb${SEM_COR}"
-
-mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SYNOLOGY_DRIVE"      -P "$DIRETORIO_DOWNLOADS"
-
-## Instalando pacotes .deb baixados na sessão anterior ##
-echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
-sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
-
-# Instalar programas no apt
-echo -e "${VERDE}[INFO] - Instalando pacotes apt do repositório${SEM_COR}"
-
-for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
-  if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    sudo apt install "$nome_do_programa" -y
-  else
-    echo "[INSTALADO] - $nome_do_programa"
-  fi
-done
-
-}
-## Instalando pacotes Flatpak ##
-install_flatpaks(){
-
-  echo -e "${VERDE}[INFO] - Instalando pacotes flatpak${SEM_COR}"
-
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub org.gimp.GIMP -y
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub org.freedesktop.Piper -y
-flatpak install flathub org.chromium.Chromium -y
-flatpak install flathub org.gnome.Boxes -y
-flatpak install flathub org.onlyoffice.desktopeditors -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
-flatpak install flathub org.flameshot.Flameshot -y
-flatpak install flathub org.electrum.electrum -y
-}
-
 ## Instalando pacotes Snap ##
 
 install_snaps(){
 
 echo -e "${VERDE}[INFO] - Instalando pacotes snap${SEM_COR}"
 
-sudo snap install authy
 
 }
 
@@ -167,40 +103,11 @@ apt_update -y
 flatpak update -y
 sudo apt autoclean -y
 sudo apt autoremove -y
-nautilus -q
 }
 
 
 # -------------------------------------------------------------------------- #
 # ----------------------------- CONFIGS EXTRAS ----------------------------- #
-
-#Cria pastas para produtividade no nautilus
-extra_config(){
-
-
-mkdir /home/$USER/TEMP
-mkdir /home/$USER/EDITAR 
-mkdir /home/$USER/Resolve
-mkdir /home/$USER/AppImage
-mkdir /home/$USER/Vídeos/'OBS Rec'
-
-#Adiciona atalhos ao Nautilus
-
-if test -f "$FILE"; then
-    echo "$FILE já existe"
-else
-    echo "$FILE não existe, criando..."
-    touch /home/$USER/.config/gkt-3.0/bookmarks
-fi
-
-echo "file:///home/$USER/EDITAR 🔵 EDITAR" >> $FILE
-echo "file:///home/$USER/AppImage" >> $FILE
-echo "file:///home/$USER/Resolve 🔴 Resolve" >> $FILE
-echo "file:///home/$USER/TEMP 🕖 TEMP" >> $FILE
-}
-
-# -------------------------------------------------------------------------------- #
-# -------------------------------EXECUÇÃO----------------------------------------- #
 
 travas_apt
 testes_internet
@@ -209,8 +116,6 @@ apt_update
 travas_apt
 add_archi386
 just_apt_update
-install_debs
-install_flatpaks
 install_snaps
 extra_config
 apt_update
